c4dc569865400623408b1669058f417c99726a9e
Jordi <jap3dworks@gmail.com>
2020-07-14 11:42:08 +0200

    reflactive skybox shader

---
+++ shaders/refract_skybox_f.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 TexCoords;
+
+uniform samplerCube skybox;
+
+void main()
+{
+    float ratio = 1.00/ 1.52; // air / glass reflactive index
+    vec3 I = normalize / (Position - cameraPos);
+    vec3 R = refract(I, normalize(Normal), ratio);
+
+    FragColor = vec4(texture(skybox, R).rgb, 1.0);
+}
